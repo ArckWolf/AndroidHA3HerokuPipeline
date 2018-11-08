5472462603c3a79f1f34e675c28c14846b945a53
ArckWolf <kevinret1@gmail.com>
2018-11-08 14:50:37 +0200

    test server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import com.jhlabs.image.BoxBlurFilter;
+
 import javax.servlet.MultipartConfigElement;
+import java.awt.image.BufferedImage;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
@@ -50,17 +53,35 @@ public class Main {
 	  // 3. Process the image using the JHLabs filtering library (you have to add the dependency)
 		// 4. Write the processed image to the HTTP response ( Tip: response.raw() can be helpful)
 	
-		post("/filter", (request, response) -> {
-			request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
-			try (InputStream is = request.raw().getPart("uploaded_file").getInputStream()) {
-				// Use the input stream to create a file
-				final Path path = Paths.get("/tmp/meh");
-				Files.copy(is, path);
+	post("/filter", (request, response) -> {
+		request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
+		try (InputStream is = request.raw().getPart("uploaded_file").getInputStream()) {
+			// Use the input stream to create a file
+			final Path path = Paths.get("/tmp/meh");
+			//Files.copy(is, path);
 
-				return "" + path;
-			}
-			//return "File uploaded";
-		});
+
+			//processImage();
+
+			return "File seen" ;
+		}
+		//return "File uploaded";
+	});
+
+
 
   }
+	private BufferedImage processImage(BufferedImage bmp) {
+		int width = bmp.getWidth();
+		int height = bmp.getHeight();
+
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
+
+		BoxBlurFilter boxBlurFilter = new BoxBlurFilter();
+		boxBlurFilter.setRadius(10);
+		boxBlurFilter.setIterations(10);
+		boxBlurFilter.filter(bmp,image);
+
+		return image;
+	}
 }
